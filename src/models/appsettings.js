b4105b72653d61fe6504881768d2044c64d27bdf
SpendBCH <SpendBCH@SpendBCH.io>
2018-09-06 18:00:34 -0700

    Add appsettings model

---
+++ src/models/appsettings.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const Appsettings = new mongoose.Schema({
+  type: { type: String, default: 'Appsettings' },
+  env: { type: String },
+  node_defaults: {
+    flavor: { type: String },
+    tier: { type: String },
+    status: { type: String },
+    private: { type: Boolean },
+    services: [{ type: String }],
+  },
+  node_flavors: [ { type: String } ],
+  usd_per_minute: { type: String },
+  minimum_billing_days: { type: Number },
+  notify_user_node_expiration_days: { type: Number },
+  quote_valid_minutes: { type: Number },
+  invoice_confirmations_required: { type: Number },
+})
+
+Appsettings.statics.getAppsettingsForEnv = async function() {
+  const env = process.env.NODE_ENV || 'development'
+  return await this.findOne({env: env}).exec()
+}
+
+module.exports = mongoose.model('appsettings', Appsettings)
