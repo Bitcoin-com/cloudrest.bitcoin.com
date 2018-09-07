1660aad1ab5b3a4100a622858d16dd61f216d0fb
SpendBCH <SpendBCH@SpendBCH.io>
2018-09-06 18:02:55 -0700

    Add mock payment-address and price services

---
+++ src/services/payment-address.js
@@ -0,0 +1,3 @@
+module.exports = async function getNewPaymentAddress () {
+  return 'bitcoincash:qpk4hk3wuxe2uqtqc97n8atzrrr6r5mleczf9sur4h'
+}
---
+++ src/services/price.js
@@ -0,0 +1,3 @@
+module.exports = async function getBchRate () {
+  return 500.10
+}
