fe8e23391e1ce03387c842ba9e4bf649fd8095e3
Gabriel Cardona <gabriel@bitcoin.com>
2018-09-14 08:36:14 +0900

    Add travis ci

--- tasks/utils.js
+++ tasks/utils.js
@@ -1,25 +1,26 @@
-const uuidv4 = require('uuid/v4')
-const replace = require('replace-in-file')
-const fs = require('fs')
-const util = require('util')
-const path = require('path')
+const uuidv4 = require("uuid/v4");
+const replace = require("replace-in-file");
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
 
 let getKubeConfig = async (templatePath, fromTokens, toValues) => {
-  templatePath = path.resolve('tasks', templatePath)
-  let tempPath = path.resolve(`tasks/temp/${uuidv4()}.json`)
-  let copyFile = util.promisify(fs.copyFile)
-  const copyResult = await copyFile(templatePath, tempPath)
+  console.log(templatePath);
+  templatePath = path.resolve("tasks", templatePath);
+  let tempPath = path.resolve(`tasks/temp/${uuidv4()}.json`);
+  let copyFile = util.promisify(fs.copyFile);
+  const copyResult = await copyFile(templatePath, tempPath);
   const replaceOptions = {
     files: tempPath,
     from: fromTokens,
-    to: toValues,
-  }
-  const fileChanges = await replace(replaceOptions)
-  const kubeConfig = require(tempPath)
-  const unlinkResult = await fs.unlink(tempPath)
-  return kubeConfig
-}
+    to: toValues
+  };
+  const fileChanges = await replace(replaceOptions);
+  const kubeConfig = require(tempPath);
+  const unlinkResult = await fs.unlink(tempPath);
+  return kubeConfig;
+};
 
 module.exports = {
-  getKubeConfig,
-}
+  getKubeConfig
+};
