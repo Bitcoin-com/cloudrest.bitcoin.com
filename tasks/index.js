f2ee945fe6e91f3570141197d3f702ffad6a579b
SpendBCH <SpendBCH@SpendBCH.io>
2018-09-13 14:03:33 -0700

    Tasks: Take regular updates of source blockchain snapshot. Get image from user's chosen flavor. Update deployment step and add backup, delete, and deleteDisk phases.

--- tasks/index.js
+++ tasks/index.js
@@ -9,6 +9,8 @@ const Invoice = require('../src/models/invoices')
 const Appsettings = require('../src/models/appsettings')
 const utils = require('./utils')
 const gcloudDisks = require('./gcloud-disks')
+const moment = require('moment')
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 async function main() {
   mongoose.Promise = global.Promise
@@ -17,6 +19,7 @@ async function main() {
 
   await processInvoices()
   await processNodes()
+  await updateBlockchainSnapshot()
   mongoose.disconnect()
 }
 
@@ -66,34 +69,130 @@ async function processNodes() {
           break
         }
 
+        // Determine node image
+        const flavor = node.flavor.split('.', 1)
+        const image = appsettings.node_flavors.find((item) => item.name == flavor).image
+        node.image = image
+
         // Update node status
         node.status = 'pending:deploying'
         await node.save()
 
         // Create deployment config files for node
         const deploymentTemplate = 'kube-templates/bch-deployment.json'
-        const fromTokens = [/#\{NODE_NAME\}#/g, /#\{DATA_DISK_NAME\}#/g]
-        const toValues = [globalNodeName, node.data_disk_name]
+        const fromTokens = [/#\{NODE_NAME\}#/g, /#\{DATA_DISK_NAME\}#/g, /#\{NODE_IMAGE\}#/g]
+        const toValues = [globalNodeName, node.data_disk_name, node.image]
         const deploymentManifest = await utils.getKubeConfig(deploymentTemplate, fromTokens, toValues)
 
         // Deploy node
-        const create = await kubeClient.apis.apps.v1.namespaces('default').deployments.post({ body: deploymentManifest })
+        const createRes = await kubeClient.apis.apps.v1.namespaces('default').deployments.post({ body: deploymentManifest })
         
         break
       case 'pending:deploying':
-        // Check rollout status
-        const deployment = await kubeClient.apis.apps.v1.namespaces('default').deployments(`${globalNodeName}-deploy`).get()
+        // Verify rollout complete to continue
+        const deployStatusRes = await kubeClient.apis.apps.v1.namespaces('default').deployments(`${globalNodeName}-deploy`).get()
+        if (deployStatusRes.body.status.readyReplicas != 1) {
+          break
+        }
 
+        // Update status
+        node.status = 'live'
+        await node.save()
+
+        break
+      case 'pending:backup':
+        // Backup data disk
+        await gcloudDisks.createSnapshotFromDisk(project, zone, node.name.toLowerCase(), node.data_disk_name)
+
+        // Backup config disk
+
+        // Update status
         node.status = 'live'
         await node.save()
 
         break
       case 'pending:delete':
+        // Update status
+        node.status = 'pending:deleteDisk'
+        await node.save()
+
         // Delete deployment & pods
         await kubeClient.apis.apps.v1.namespaces('default').deployments(`${globalNodeName}-deploy`).delete()
+
+        break
+      case 'pending:deleteDisk':
+        // Verify disk is detached to continue
+        const isDiskAttached = await gcloudDisks.isDiskAttached(project, zone, sourceDiskName)
+        if (isDiskAttached) {
+          break
+        }
+
+        // Delete disk
+        await gcloudDisks.deleteDisk(project, zone, node.data_disk_name)
+
+        // Delete backups
+
+        // Update status
+        node.status = 'deleted'
+        await node.save()
+
         break
     }
   }
+}
+
+async function updateBlockchainSnapshot() {
+  // Fetch current appsettings
+  const appsettings = await Appsettings.getAppsettingsForEnv()
+  const project = appsettings.gcloud.project
+  const zone = appsettings.gcloud.zone
+  const sourceDeploy = appsettings.source_blockchain_deploy
+  const sourceDiskName = appsettings.source_blockchain_disk
+
+  // Verify snapshot is stale to continue
+  const snapshotRes = await gcloudDisks.getSnapshot(project, appsettings.source_blockchain_snapshot)
+  const createdAt = moment(snapshotRes.data.creationTimestamp)
+  const expiresAt = createdAt.add(24, 'hours')
+  const now = moment()
+  if (now.isBefore(expiresAt)) {
+    return
+  }
+
+  // Scale down source deployment
+  const scaleDownRes = await kubeClient.apis.apps.v1.namespaces('default').deployments(sourceDeploy).scale.patch({ body: { spec: { replicas: 0 } } })
+
+  // Wait for deployment to scale down
+  while (true) {
+    const isDiskAttached = await gcloudDisks.isDiskAttached(project, zone, sourceDiskName)
+
+    if (!isDiskAttached) {
+      break
+    } else {
+      await sleep(1000)
+    }
+  }
+
+  // Create snapshot
+  const snapshotName = `bch-data-${now.toISOString().toLowerCase().replace(/\:|\./g, '-')}`
+  await gcloudDisks.createSnapshotFromDisk(project, zone, snapshotName, sourceDiskName)
+
+  // Wait for snapshot to complete
+  while (true) {
+    const isSnapshotReady = await gcloudDisks.isSnapshotReady(project, snapshotName)
+    if (isSnapshotReady) {
+      break
+    } else {
+      await sleep(1000)
+    }
+  }
+
+  // Resume source deploy
+  const scaleUpRes = await kubeClient.apis.apps.v1.namespaces('default').deployments(sourceDeploy).scale.patch({ body: { spec: { replicas: 1 } } })
+
+  // Update current snapshot in appsettings
+  let appsettingsToUpdate = await Appsettings.getAppsettingsForEnv()
+  appsettingsToUpdate.source_blockchain_snapshot = snapshotName
+  await appsettingsToUpdate.save()
 }
 
 async function processExpiringNodes() {
